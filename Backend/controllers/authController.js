b2674d39abdbaff13d723b82a5fcebab88b1b309
ombhut27 <122210267+ombhut27@users.noreply.github.com>
2025-06-04 10:28:59 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
-import transporter from '../config/nodemailer.js'
+import transporter from '../Config/nodemailer.js'
 
 
 export const register = async (req, res) => {
